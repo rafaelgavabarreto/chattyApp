8616e2e2b2c65eca0e2011e3ce6cbd04d82a5360
Rafael Barreto <rafaelgavabarreto@gmail.com>
2018-06-12 17:39:55 +0000

    Passing messages as props

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -8,13 +8,8 @@ class MessageList extends Component {
         {
           this.props.messages.map((chatObj, index) =>
             <Message index= {index}
-<<<<<<< HEAD
             chattyUsername= {chattyObj.username}
             chattyMessage={chattyObj.content} />)
-=======
-            chattyUsername= {chatObj.username}
-            chattyMessage={chatObj.content} />)
->>>>>>> 1287d3f6888baaa2eab167ee7fa1150fe5bed26d
         }
         <div className="message system"/>
       </main>
